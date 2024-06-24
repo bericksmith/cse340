ad38cbb38f4cfff5a3ce37b9eabd2dcb93860326
Berick Smith <bericksmith@gmail.com>
2024-06-23 23:20:22 -0700

    W04 Login Account Work

--- server.js
+++ server.js
@@ -15,6 +15,35 @@ const inventoryRoute = require("./routes/inventoryRoute")
 const utilities = require("./utilities/index")
 const errorRoute = require("./routes/errorRoute")
 const errorHandler = require("./routes/errorRoute")
+const session = require("express-session")
+const pool = require('./database/')
+const accountRoute = require("./routes/accountRoute")
+const bodyParser = require("body-parser")
+
+/* ***********************
+ * Middleware
+ * ************************/
+app.use(session({
+  store: new (require('connect-pg-simple')(session))({
+    createTableIfMissing: true,
+    pool,
+  }),
+  secret: process.env.SESSION_SECRET,
+  resave: true,
+  saveUninitialized: true,
+  name: 'sessionId',
+}))
+
+// Express Messages Middleware
+app.use(require('connect-flash')())
+app.use(function(req, res, next){
+  res.locals.messages = require('express-messages')(req, res)
+  next()
+})
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+
 /* ***********************
  * View Engine and Templates
  *************************/
@@ -30,6 +59,8 @@ app.use(static)
 app.get("/", utilities.handleErrors(baseController.buildHome))
 // Inventory routes
 app.use('/inv', utilities.handleErrors(inventoryRoute))
+// Intentional Error route
+app.use('/account', utilities.handleErrors(accountRoute))
 // Intentional Error route
 app.use('/error', utilities.handleErrors(errorRoute))
 // File Not Found Route - must be last route in list
